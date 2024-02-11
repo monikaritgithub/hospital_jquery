dff4ff2166f68cc0f02ebb9afd81955fed5d8130
Smriti Bhandari <moonekabhandari123@gmail.com>
2024-02-11 06:37:05 -0900

    hospital_jquery

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Smooth scroll for navigation links
+    $('nav a').on('click', function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 800);
+    });
+});
